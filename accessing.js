e2214dcf5d5d24dd5c35405ae0d54244d37e1e1f
Pearlynne <chongpearlynnelh@gmail.com>
2023-11-07 19:52:43 -0800

    Added partial solutions from in-class exercises

--- accessing.js
+++ accessing.js
@@ -1,5 +1,19 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
 
+//<h2 id="weather-head">Seattle Weather</h2>
+
+const weatherHead = document.getElementById("weather-head");
+
+weatherHead.innerText = "February 10 Weather Forecast, Seattle";
+
 // Change the styling of every element with class "sun" to set the color to "orange"
 
+const sun = document.getElementsByClassName("sun");
+
+for (i = 0; i < sun.length; i++){
+	sun[i].style.color = "orange";
+}
+
 // Change the class of the second <li> from to "sun" to "cloudy"
+
+sun[1].className = "cloudy";
--- dom-crud.js
+++ dom-crud.js
@@ -1,14 +1,36 @@
 // Create a new <a> element containing the text "Buy Now!" 
 // with an id of "cta" after the last <p>
 
+const aEl = document.createElement('a');
+
+const aTextNode = document.createTextNode(`Buy Now!`);
+
+aEl.setAttribute('id',"cta");
+aEl.appendChild(aTextNode);
+
+const paragraph = document.querySelector('p');
+paragraph.after(aEl);
+
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
 
+const imgClass= document.querySelector("img")
+imgColor = imgClass.getAttribute('data-color')
+
+console.log(imgColor)
 
 // Update the third <li> item ("Turbocharged"), 
 // set the class name to "highlight"
 
+const li = document.getElementsByTagName('li')
+
+li[2].setAttribute('class',"highlight");
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+
+const main = document.getElementsByTagName('main')
+
+// Got this from getting path
+main[0].removeChild(paragraph)
