32fb16dedf37032db5d08c35fe6204d1368d01eb
Pearlynne <chongpearlynnelh@gmail.com>
2023-11-08 12:49:32 -0800

    Add eventlistener to print coordinates

--- wheresThePointer.js
+++ wheresThePointer.js
@@ -1,3 +1,29 @@
 // Attach one listener that will detect clicks on any of the <td>
 // elements.  Should update that element's innerHTML to be the
 // x, y coordinates of the mouse at the time of the click
+
+const td = document.getElementsByTagName("td")
+
+let cellIndex = 0
+
+const clickTable = function (e) {
+	// get mouse coordinates
+	let xCoord = e.clientX;
+	let yCoord = e.clientY;
+
+	// To fix: remove current innerHTML via eventlistener instead
+	td[cellIndex].innerHTML = "";
+
+	// Loop to find which cell was clicked
+	for (i = 0; i < td.length; i++) {
+		// Add to inner HTML to cell
+		if (e.target == td[i]) {
+			let clickedCell = td[i];
+			cellIndex = i;
+			clickedCell.innerHTML = `${xCoord}, ${yCoord}`;
+			break;
+		}
+	}
+}
+
+document.addEventListener("click", clickTable);
