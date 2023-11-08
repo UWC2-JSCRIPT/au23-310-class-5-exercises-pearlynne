ae7437c556e13846b4aaeced072a58fe1118a4d3
Pearlynne <chongpearlynnelh@gmail.com>
2023-11-08 13:08:58 -0800

    Format and add comments

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -5,18 +5,17 @@ document.getElementById('cat').addEventListener('click', () => {
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
-
 document.querySelector('a#more-info').addEventListener('click', (e) => {
-	alert(`Here's some info!`)
+	alert(`Here's some info!`);
 	// Stop deafault in html
 	e.preventDefault();
-})
+});
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
 document.getElementById('dog').addEventListener('click', (e) => {
 	alert('Bow wow!');
-	
+
 	// Stop propagating meow alert
-	e.stopPropagation()
+	e.stopPropagation();
 });
