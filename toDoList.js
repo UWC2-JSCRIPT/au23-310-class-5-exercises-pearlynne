7f3f3b99a8b240f5fdf9f7eb02f3ff45029f1362
Pearlynne <chongpearlynnelh@gmail.com>
2023-11-13 18:18:28 -0800

    Attempt reordering list and added up-down buttons

--- toDoList.js
+++ toDoList.js
@@ -3,27 +3,27 @@
 let toDoList = document.querySelector('ul.today-list');
 
 toDoList.addEventListener(('click'), (e) => {
-		if (e.target.nodeName.toLowerCase() == 'li' ) {
-			e.target.classList.toggle('done');
-		};
+	if (e.target.nodeName.toLowerCase() == 'li') {
+		e.target.classList.toggle('done');
+	}
 
-		// Span needs to be included (find way to include parent)
-		if (e.target.nodeName.toLowerCase() == 'span' ) {
-			e.target.parentNode.classList.toggle('done');
-		};
-	});
+	// Span needs to be included (find way to include parent)
+	if (e.target.nodeName.toLowerCase() == 'span') {
+		e.target.parentNode.classList.toggle('done');
+	}
+})
 
 // Selecting only list elements will not work for 
-// new items as function was loaded after
+// new items as function was loaded after 
+// unlesss we add event listener in function
 
 
 // If a delete link is clicked, delete the li element / remove from the DOM
-
 toDoList.addEventListener('click', (e) => {
-	if(e.target.className == 'delete' )		
+	if (e.target.className == 'delete') {
 		e.target.parentElement.remove();
-		// e.stopPropagation();
-	});
+	}
+})
 
 
 // If an 'Add' link is clicked, adds the item as a new list item with
@@ -46,16 +46,65 @@ const addListItem = function (e) {
 	const newSpanItem = document.createElement('span');
 	newSpanItem.textContent = `${text}\n`;
 
-	// Append <span> and a.delete elements to <li> element
-	newLiItem.append(newSpanItem, newAItem);
+	// Extra Credit: Create new sorting buttons
+	const sortButtons = createButtons();
+
+	// Append <span> and a.delete and moving buttons to <li> element
+	newLiItem.append(newSpanItem, newAItem, sortButtons.upLink, sortButtons.downLink);
 
 	// Append new list element to unordered list
 	toDoList.appendChild(newLiItem);
 
 	// Reset value
 	input.value = '';
-};
+}
 
 // Add eventlistener to add-item class
 const add = document.querySelector('a.add-item');
 add.addEventListener('click', addListItem);
+
+
+/* -------------------- Extra Credit -------------------- */
+
+//  Create new move up/down buttons
+const createButtons = (function () {
+	// Create new Move Up button
+	const upLink = document.createElement('button');
+	upLink.setAttribute("class", "sortUp");
+	upLink.textContent = `Move up \n`;
+
+	// Create new Move Down button
+	const downLink = document.createElement('button');
+	downLink.setAttribute("class", 'sortDown');
+	downLink.textContent = `Move down \n`;
+
+	return { upLink: upLink, downLink: downLink };
+})
+
+
+// Add buttons to current list (to fix: more efficient)
+let liEl = document.querySelectorAll('li');
+liEl.forEach((item) => {
+	let sortButtons = createButtons();
+	item.append(sortButtons.upLink, sortButtons.downLink);
+})
+
+
+// Functions to reorder buttons 
+toDoList.addEventListener('click', (e) => {
+	// Get list item
+	let item = e.target.parentNode;
+
+	// Move list item up or down 
+	if (e.target.className == 'sortUp') {
+		// Insert if there is a previous sibling
+		if (item.previousElementSibling) {
+			item.parentNode.insertBefore(item, item.previousElementSibling);
+		}
+	} else if (e.target.className == 'sortDown') {
+		// Insert if there is a next sibling
+		if (item.nextElementSibling) {
+			item.parentNode.insertBefore(item.nextElementSibling, item);
+		}
+	}
+})
